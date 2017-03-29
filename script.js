93e92a1bbbfc9a20dbbb96d28363f0ea4d885a66
Maggie Mau <meander@vt.edu>
2017-03-29 19:58:39 -0400

    get it together

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ function squared()
         document.getElementById("result").innerHTML = num1 * num1;
 }
 
+function cubed()
 {
         num1 = document.getElementById("firstNumber").value;
         document.getElementById("result").innerHTML = num1 * num1 * num1;
