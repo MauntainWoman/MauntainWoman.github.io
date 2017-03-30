a2910944d6e627b29bcafe56352c2d8cb648b5e9
Maggie Mau <meander@vt.edu>
2017-03-29 20:09:26 -0400

    oh god I broke it

--- script.js
+++ script.js
@@ -1,11 +1,5 @@
-function squared()
-{
-        num1 = document.getElementById("firstNumber").value;
-        document.getElementById("result").innerHTML = num1 * num1;
-}
-
 function cubed()
 {
         num1 = document.getElementById("firstNumber").value;
-        document.getElementById("result").innerHTML = num1 * num1 * num1;
+        document.getElementById("result").innerHTML = num1 * num1;
 }
