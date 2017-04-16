e4bb1f0d193c8720eff255caa9cca96278a248e6
Maggie Mau <meander@vt.edu>
2017-04-16 12:16:12 -0400

    adding some exceptions

--- numberGuesser/numberGuesser.js
+++ numberGuesser/numberGuesser.js
@@ -5,23 +5,32 @@ var previousGuess = 0;
 var number = Math.floor(Math.random() * 100 + 1);
 
 function numberGuesser(){
-   var currentGuess = document.getElementById("userInput").value;
-   if (currentGuess == number) { // If the numbers match
-     document.getElementById("guess").innerHTML = "You guessed " + currentGuess;
-     document.getElementById("highOrLow").innerHTML = "Correct! The number I was thinking was " + currentGuess;
-   } else if (currentGuess < number && previousGuess < number) { // If you guessed too low both times
-     document.getElementById("guess").innerHTML = "You guessed " + currentGuess;
-     document.getElementById("highOrLow").innerHTML = "The number I am thinking of is higher than " + currentGuess + " and higher than " + previousGuess;
-   } else if (currentGuess < number && previousGuess > number) { // If you guessed too high then too low
-     document.getElementById("guess").innerHTML = "You guessed " + currentGuess;
-     document.getElementById("highOrLow").innerHTML = "The number I am thinking of is in between " + currentGuess + " and " + previousGuess;
-   } else if (currentGuess > number && previousGuess > number) { //If you guessed too high both times
-     document.getElementById("guess").innerHTML = "You guessed " + currentGuess;
-     document.getElementById("highOrLow").innerHTML = "The number I am thinking of is lower than " + currentGuess + " and lower than " + previousGuess;
-   }
-   else if (currentGuess > number && previousGuess < number) { //If you guessed too low then too high
+  var currentGuess = document.getElementById("userInput").value;
+  if (currentGuess == ""){
+    document.getElementById("highOrLow").innerHTML = "You actually have to  guess a number, dummy";
+    currentGuess = previousGuess;
+  } else if (currentGuess < 0 || currentGuess > 100){
+    document.getElementById("highOrLow").innerHTML = "Enter a number between 0 and 100";
+    currentGuess = previousGuess;
+  } else if (currentGuess == number) { // If the numbers match
+    document.getElementById("guess").innerHTML = "You guessed " + currentGuess;
+    document.getElementById("highOrLow").innerHTML = "Correct! The number I was thinking was " + currentGuess;
+    previousGuess = currentGuess;
+  } else if (currentGuess < number && previousGuess < number) { // If you guessed too low both times
+    document.getElementById("guess").innerHTML = "You guessed " + currentGuess;
+    document.getElementById("highOrLow").innerHTML = "The number I am thinking of is higher than " + currentGuess + " and higher than " + previousGuess;
+    previousGuess = currentGuess;
+  } else if (currentGuess < number && previousGuess > number) { // If you guessed too high then too low
+    document.getElementById("guess").innerHTML = "You guessed " + currentGuess;
+    document.getElementById("highOrLow").innerHTML = "The number I am thinking of is in between " + currentGuess + " and " + previousGuess;
+    previousGuess = currentGuess;
+  } else if (currentGuess > number && previousGuess > number) { //If you guessed too high both times
+    document.getElementById("guess").innerHTML = "You guessed " + currentGuess;
+    document.getElementById("highOrLow").innerHTML = "The number I am thinking of is lower than " + currentGuess + " and lower than " + previousGuess;
+    previousGuess = currentGuess;
+  } else if (currentGuess > number && previousGuess < number) { //If you guessed too low then too high
     document.getElementById("guess").innerHTML = "You guessed " + currentGuess;
     document.getElementById("highOrLow").innerHTML = "The number I am thinking of is in between " + previousGuess + " and " + currentGuess;
+    previousGuess = currentGuess;
   }
-   previousGuess = currentGuess;
 }
